54e69cf11693d9d1b22b91f9ac8f873a658a388d
Mhassan0001 <hassanfareed5050@gmail.com>
2026-04-02 23:58:29 +0500

    feat: setup React Router v7 with initial routes (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <p className="text-3xl font-bold bg-blue-500 underline text-center">Tailwind Load Successfully.....</p>
-      </div>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
