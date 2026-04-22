1b27784360b3db51bc00ea9a62427201e8042aa1
Mhassan0001 <hassanfareed5050@gmail.com>
2026-04-22 22:10:57 +0500

    WIP: dashboard UI updates

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { FaMessage } from "react-icons/fa6";
-
+import { IoIosContacts } from "react-icons/io";
+import { IoMdCall } from "react-icons/io";
+import { MdOutlineLogout } from "react-icons/md";
 const Dashboard = () => {
   return (
     <>
       <div className="grid grid-cols-[4rem_23.75rem_52.25rem] min-h-screen ">
-        <div className= " bg-black ">
-         
+        <div className=" bg-black flex  flex-col items-center gap-10 h-full py-7 ">
+          <div className="x">
+            <FaMessage size={25} className="text-[#8B5CF6]" />
+          </div>
+          <div>
+            <IoIosContacts size={30} className="text-white" />
+          </div>
+          <div className="">
+            <IoMdCall size={30} className="text-white " />
+          </div>
+
+          <div className="mt-auto">
+            <MdOutlineLogout size={30} className="text-white " />
+          </div>
         </div>
+
+        <div className="bg-[#0C0C0E]"></div>
       </div>
     </>
   );
