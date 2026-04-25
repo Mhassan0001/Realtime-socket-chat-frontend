4fb63bdb840881d05f12acb823c47f23d04d36d7
Mhassan0001 <hassanfareed5050@gmail.com>
2026-04-25 23:55:33 +0500

    feat(search): implement search filtering using local mock data (API pending)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaMessage } from "react-icons/fa6";
 import { IoIosContacts } from "react-icons/io";
 import { IoMdCall } from "react-icons/io";
 import { MdOutlineLogout } from "react-icons/md";
+
 const Dashboard = () => {
+  const contacts = [
+    { id: 1, name: "Ali Raza", number: "03001234567" },
+    { id: 2, name: "Sara Khan", number: "03119876543" },
+    { id: 3, name: "Ahmed Malik", number: "03225558888" },
+    { id: 4, name: "Fatima Noor", number: "03339994444" },
+  ];
+
+  const [query, setQuery] = useState("");
+  const [filter, setFilter] = useState([]);
+
+  const handleSearch = (e) => {
+    let value = e.target.value;
+    setQuery(value);
+
+    if (value.trim() === "") {
+      setFilter([]);
+    } else {
+      const result = contacts.filter((contact) =>
+        contact.number.includes(value),
+      );
+
+      setFilter(result);
+    }
+  };
   return (
     <>
       <div className="grid grid-cols-[4rem_23.75rem_52.25rem] min-h-screen ">
@@ -30,11 +55,30 @@ const Dashboard = () => {
 
           <p className="pl-7">
             <input
-              type=""
-              className="focus:outline-none text-[#A9ABB3] custom-dashIn focus:ring-2 focus:ring-purple-400 "
+              value={query}
+              onChange={handleSearch}
+              type="text"
+              className="focus:outline-none text-[#818181] custom-dashIn focus:ring-2 focus:ring-purple-400 "
               placeholder="Find Contact's"
             />
           </p>
+
+          {filter.length > 0 && (
+            <div className="ps-5">
+              {filter.map((contact) => {
+                return (
+                  <li className="text-white">
+                    <h1>{contact.name}</h1>
+                    <h1>{contact.number}</h1>
+                  </li>
+                );
+              })}
+            </div>
+          )}
+
+
+          
+
         </div>
       </div>
     </>
